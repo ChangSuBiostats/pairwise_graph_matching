88a2f5caa0afffe06d2fc03f6442f571fe1d0e6e
Chang <c.su@yale.edu>
2021-04-10 15:47:19 -0400

    feat: add default pars; save useful summary stats

--- match_to_a_subj.m
+++ match_to_a_subj.m
@@ -1,39 +1,88 @@
-function []=match_to_a_subj(i_ref, lambda, penalty)
+function []=match_to_a_subj(i_ref, lambda, penalty, data_path)
     arguments
-        i_ref (1,1) {mustBeNumeric,mustBeReal}
+        i_ref (1,1) {mustBeNumeric,mustBeReal} = 1
         lambda (1,1) {mustBeNumeric,mustBeReal} = 3e-4
-        penalty (1,1) char {mustBeMember(penalty,{'vanilla','two_region'})} = 'vanilla'
+        penalty (1,:) char {mustBeMember(penalty,{'vanilla','two_region'})} = 'two_region'
+        data_path (1,:) char = 'data/'
     end
     
+    fprintf('i_ref=%i, lambda=%.1e, penalty=%s, data_path=%s \n', i_ref, lambda, penalty, data_path)
+    
     % reference: 
-    % make MATLAB take arguments from command line:
+    % 1. make MATLAB take arguments from command line:
     %   https://stackoverflow.com/questions/8981168/running-a-matlab-program-with-arguments
-    % set default value for parameters
+    % 2. set default value for parameters
     %   https://www.mathworks.com/help/matlab/matlab_prog/function-argument-validation-1.html
+    % 3. save large sparse matrix efficiently
+    %   reference: https://www.mathworks.com/matlabcentral/answers/101798-how-can-i-write-a-sparse-matrix-s-non-zero-values-and-the-corresponding-row-and-column-information-t
+        
     
-    % load codes for graph matching
-    addpath /Users/chang/Documents/research/brain_connectivity/code
+    %% set up paths and directories
+    % add the path where data are stored
+    addpath(data_path)
     
-    % load data
-    test = load('/Users/chang/Documents/research/brain_connectivity/data/precision/cc400_regFCprec_concat_hpf_997subj.mat');
-    retest = load('/Users/chang/Documents/research/brain_connectivity/data/precision/test_retest/cc400_regFCprec_concat_hpf_retest41.mat');
+    if ~exist('output', 'dir')
+       fprintf('directory ./output created \n')
+       mkdir('output')
+    end
+    
+    if ~exist('output/matching_results', 'dir')
+       fprintf('directory ./output/matching_results created \n')
+       mkdir('output/matching_results')
+    end
+    
+    %% load data
+    test = load('cc400_regFCprec_concat_hpf_997subj.mat');
+    % retest = load('cc400_regFCprec_concat_hpf_retest41.mat');
     n = size(test.subj, 2);
     
-    % design a penalty matrix
-    if penalty == 'vanilla'
+    %% start matching
+    
+    % set penalty matrices
+    if strcmp(penalty, 'vanilla')
+        penalty_m = load('output/vanilla_penalty.mat').vanilla_p;
+    elseif strcmp(penalty, 'two_region')
+        penalty_m = load('output/two_regions_penalty.mat').two_region_p;
+    end    
     
     % set the FC map from subj i as the reference map
     m_ref = squeeze(test.C(i_ref, :, :));
-    m_ref(logical(eye(392))) = 0;
+    m_ref(logical(eye(392))) = 0; % remove the effect of diagonal values
     
+    % set number of iterations to 1 to save computational time
+    n_iter = 1;
+    
+    % keep track of
+    % 1) the position of nonzero entries in the permutation matrix
+    swap_positions = zeros(n, 392, 2);
+    % 2) difference between two matrices before and after permutation
+    diff = zeros(n,2); 
+    % 3) number of off diagonal swaps
+    offdiag_swap_counts = zeros(n,1); 
+    % 4) array of permutation matrices 
+    P_array = zeros(392, 392, n);
+    
+    % Loop through all pairs 
+    % Though the computation is redundant (some pairs might have been
+    % matched before),
+    % we stick with this because the data will be eaiser to access.
     for i = 1:n
         m2 = squeeze(test.C(i, :, :));
         m2(logical(eye(392))) = 0;
-        [P, diff, obj, new_m] = iterative_procedure(m_ref, m2, n_iter, lambda, vanilla_p);
-        P_cross{1,i} = P;
-        diff_m_cross(i) = diff(n_iter + 1);
-        off_diag_matches_cross(1, i) = 392 - sum(P_cross{1, i}(logical(eye(392))));
+        [P, d, ~, ~] = iterative_procedure(m_ref, m2, n_iter, lambda, penalty_m);
+        diff(i,:) = d;
+        offdiag_swap_counts(i) = 392 - sum(P(logical(eye(392))));
+        [row, col, ] = find(P);
+        % temporarily store the permutation matrices
+        P_array(:, :, i) = P;
+        swap_positions(i, :, :) = [row, col];
     end
+    
+    % sum of permutation matrices
+    [row, col, v] = find(sum(P_array, 3));
+    % the positions and values of nonzero entries
+    sum_swaps = [row, col, v];
 
-
+    save(strcat('output/matching_results/P_', num2str(i_ref), '.mat'), 'swap_positions', 'sum_swaps', 'diff', 'offdiag_swap_counts');
 end
+
