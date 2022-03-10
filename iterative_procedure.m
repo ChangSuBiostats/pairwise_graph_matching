6fae30ba5e2a2dd004e6d1fc7ee826b3fe3c0c05
Chang <c.su@yale.edu>
2022-03-10 10:36:48 -0500

    feat: implement eps stopping rule; return sum of swaps and obj function for each iteration

--- iterative_procedure.m
+++ iterative_procedure.m
@@ -1,4 +1,4 @@
-function [P_old, diff, obj, new_m, cost_m] = iterative_procedure(m1, m2, n_iter, lambda, penalty_m)
+function [P_old, diff, obj, new_m, cost_m, sum_swaps] = iterative_procedure(m1, m2, n_iter, lambda, penalty_m)
 % match two maps using the iterative procedure, with possile regularization by penalizing certain matches
 % Arguments:
 %   m1: map 1
@@ -21,6 +21,7 @@ function [P_old, diff, obj, new_m, cost_m] = iterative_procedure(m1, m2, n_iter,
     
     diff = zeros(1,n_iter+1);
     obj = zeros(1,n_iter+1);
+    sum_swaps = zeros(1, n_iter+1);
                                                                                 
     %% initiate the iterative procedure
     
@@ -28,22 +29,37 @@ function [P_old, diff, obj, new_m, cost_m] = iterative_procedure(m1, m2, n_iter,
 
     %% objective function
     diff(1,1) = norm(P_old' * m1 * P_old - m2,'fro')^2;
-    obj(1,1) = diff(1,1) + lambda * trace(P_old' * penalty_m);
+    obj(1,1) = trace(P_old' * (m1' * P_old * m2 - lambda * penalty_m));
+    sum_swaps(1,1) = 0;
     
     %% iterative updates
     % tic
-    for iter = 1:n_iter
+    % stoppint criterion
+    eps = 0.001;
+    rdelta = 1;
+    iter = 1;
+    % for iter = 1:n_iter
+    while rdelta > eps && iter <= n_iter
         % -------------------
         M = matchpairs(m1' * P_old * m2 - lambda * penalty_m, -99999, 'max');
         P_new = full(sparse(M(:, 1), M(:, 2), 1, n_dim, n_dim)); % adjust the format
+        obj(1, iter + 1) = trace(P_new' * (m1' * P_old * m2 - lambda * penalty_m));
         P_old = P_new;
         % -------------------
         diff(1, iter + 1) = norm(P_old' * m1 * P_old - m2, 'fro')^2;
-        obj(1, iter + 1) = diff(1, iter + 1) + lambda * trace(P_old' * penalty_m);
+        tmp = P_old - diag(diag(P_old));
+        sum_swaps(1, iter + 1) = sum(tmp, 'all');
+        % stopping rule: difference <= epsilon
+        rdelta = obj(1, iter + 1) - obj(1, iter);
+        iter = iter + 1;
     end
     % toc
+    
+    if iter > n_iter
+        fprintf('Iterative proceure did not converge in %i iterations.', i_ref)    
+    end
 
     new_m = P_old' * m1 * P_old;
     
     % new: evaluate the cost matrix used in the Hungarian algorithm
-    cost_m = m1' * P_old * m2 - lambda * penalty_m
+    cost_m = m1' * P_old * m2 - lambda * penalty_m;
